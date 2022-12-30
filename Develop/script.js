da4698748606923d1fb29365c47b290ba0b2da37
ecinematic <ecinematic@yahoo.com>
2022-12-29 20:46:48 -0600

    added prompt for number of characters

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,18 @@
 //Generate the Password
 function generatePassword() {
   var finalPassword = "";
+  // input of how many characters
+  var numberOfCharacters = prompt("Please enter the number of characters");
+  console.log(numberOfCharacters);
+  // review to ensure it is between 8 to 128 characters
+  if (numberOfCharacters < 8) {
+    alert("Must be greater than 8 characters");
+    return;
+  }
+  if (numberOfCharacters > 128) {
+    alert("Must be less than 128 characters");
+    return;
+  }
   finalPassword = "test";
   return finalPassword;
 }
