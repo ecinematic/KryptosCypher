af35e25377a078f277f2255e33645628ada8d742
ecinematic <ecinematic@yahoo.com>
2022-12-30 11:55:18 -0600

    consolidated if statements and added random lowercase password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+// Array of Character Options
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacter = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //Generate the Password
 function generatePassword() {
@@ -8,15 +12,29 @@ function generatePassword() {
   var numberOfCharacters = prompt("Please enter the number of characters");
   console.log(numberOfCharacters);
   // review to ensure it is between 8 to 128 characters
-  if (numberOfCharacters < 8) {
-    alert("Must be greater than 8 characters");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    alert("Must be between 8 and 128 characters");
     return;
   }
-  if (numberOfCharacters > 128) {
-    alert("Must be less than 128 characters");
-    return;
-  }
-  finalPassword = "test";
+  //prompt for password criteria
+    lCase = confirm("Do you want lowercase letters?");
+    uCase = confirm("Do you want uppercase letters?");
+    num = confirm("Do you want numbers?");
+    sCharacters = confirm("Do you want special characters?")
+    
+    console.log(typeof lCase);
+    console.log(lCase);
+
+    // select a random lowercase 
+    for (var i = 0; i < numberOfCharacters; i++) {
+      if (lCase === true) {
+       var lc = alphaLowerCase[Math.floor(Math.random() * 26)];
+       console.log(lc);
+       finalPassword = finalPassword + lc;
+       console.log(finalPassword);
+     }
+     }
+
   return finalPassword;
 }
 
